17e783abed50795a55b148f270254ecba550ea31
Othmane Benkirane <eo@rhyhann.net>
2008-07-04 21:20:19 +0100

    Little flash problem

--- lib/authenticate.rb
+++ lib/authenticate.rb
@@ -1,12 +1,8 @@
 module Authenticate
   def authenticate
-    authenticated? || deny_authentication
+    authenticated?
   end
   def authenticated?
     session[:password] ? (CONFIG['password'] == session[:password]) : false
-  end
-  def deny_authentication
-    flash[:now] = "Authentification refusée"
-    #redirect_to request.request_uri
   end
 end
